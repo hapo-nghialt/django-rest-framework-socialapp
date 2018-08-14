47fe77ff74717c0d860967422d8b50d8f19238aa
Haposoft_Nghia <hapo_git@haposoft.com>
2018-08-14 09:54:09 +0700

    Series 2: posts app

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Post(models.Model):
+    content = models.TextField()
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def __str__(self):
+        return self.content
+
+    class Meta:
+        ordering = ["timestamp", "updated"]
